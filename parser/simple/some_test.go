6029e215f1e8f6b0836b5b791359151d4b2775fe
nfk93 <nfk1993@gmail.com>
2020-05-26 23:54:15 +0200

    restructure testcode

--- parser/simple/some_test.go
+++ parser/simple/some_test.go
@@ -12,11 +12,8 @@ import (
 	"github.com/nfk93/gocap/parser/simple/parser"
 )
 
-func TestAll(t *testing.T) {
-  // var files []string
-  // gopath := strings.Split(os.Getenv("GOPATH"), string(os.PathListSeparator))
-  // root := gopath + "/src/github.com/nfk93/gocap/parser/tests"
-  root := "../tests"
+func TestAllGood(t *testing.T) {
+  root := "../tests/success"
   items, _ := ioutil.ReadDir(root)
   for _, file := range items {
     t.Run(file.Name(), func(t *testing.T) {
@@ -36,33 +33,22 @@ func TestAll(t *testing.T) {
   }
 }
 
-// func TestUnsupportBufferedChannel(t *testing.T) {
-// 	dat, err := ioutil.ReadFile("../tests/unsupport_buffered_chan.cgo")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	lex := lexer.NewLexer(dat)
-// 	p := parser.NewParser()
-// 	_, errParse := p.Parse(lex)
-// 	if errParse == nil {
-// 		t.Error("should have failed due to buffered channel")
-// 	}
-// }
-//
-// func TestWorld(t *testing.T) {
-// 	dat, err := ioutil.ReadFile("../tests/test1.go")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	lex := lexer.NewLexer(dat)
-// 	p := parser.NewParser()
-// 	s, errParse := p.Parse(lex)
-// 	fmt.Println(s)
-// 	fmt.Println(123)
-// 	if errParse != nil {
-// 		panic(errParse)
-// 	}
-//
-// }
+func TestAllBad( t *testing.T) {
+  root := "../tests/bad"
+  items, _ := ioutil.ReadDir(root)
+  for _, file := range items {
+    t.Run(file.Name(), func(t *testing.T) {
+      dat, err := ioutil.ReadFile(root + "/" + file.Name())
+      if err != nil {
+        panic(err)
+      }
+
+      lex := lexer.NewLexer(dat)
+      p := parser.NewParser()
+      s, errParse := p.Parse(lex)
+      if errParse == nil {
+        t.Error(s.(ast.Code).ToString())
+      }
+    })
+  }
+}
